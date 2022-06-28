32378781b0592a07cae95dec0e9aae3cbb142447
Ozge <erolozge01@gmail.com>
2022-06-28 09:43:53 -0500

    add, remove, list and read notes

---
+++ notes-app/notes.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const getNotes = function () {
+  return "Your notes...";
+};
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  //debugger;
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.bold.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.bold.inverse("Note title taken!"));
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.bold.inverse("Note removed!"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.bold.inverse("No note found!"));
+  }
+};
+
+const listNotes = function () {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your Notes :"));
+  notes.forEach((note) => console.log(note.title));
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+};
